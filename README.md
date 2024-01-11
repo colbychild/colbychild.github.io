5d09dc6826bb81c98427842651dc0b06f1ca1022
colbychild <155912404+colbychild@users.noreply.github.com>
2024-01-10 20:38:18 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# colbychild.github.io
+Professional Website
---
+++ javascript.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('.scroll-link');
+    const nav = document.querySelector('nav');
+
+    function highlightNavLinks() {
+        let fromTop = window.scrollY + nav.offsetHeight + 120;
+
+        navLinks.forEach(link => {
+            const section = document.querySelector(link.getAttribute('href'));
+
+            if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
+                navLinks.forEach(link => link.classList.remove('active'));
+                link.classList.add('active');
+            }
+        });
+    }
+
+    window.addEventListener('scroll', highlightNavLinks);
+    // Smooth scrolling for anchor links
+    navLinks.forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // Initial call to highlightNavLinks to set the initial state
+    highlightNavLinks();
+    });
+    
+    // Show/hide the Scroll to Top button based on scroll position
+        window.addEventListener('scroll', function () {
+            const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                scrollToTopBtn.style.display = 'block';
+            } else {
+                scrollToTopBtn.style.display = 'none';
+            }
+        });
+
+        // Easing function: easeInOutQuad
+        function easeInOutQuad(t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        }
+
+        // Smooth Scroll to Top function with easing
+        function scrollToTop() {
+            const startTime = performance.now();
+            const duration = 2500; // milliseconds
+
+            function scrollStep() {
+                const currentTime = performance.now();
+                const timeElapsed = currentTime - startTime;
+
+                window.scrollBy(0, easeInOutQuad(timeElapsed, 0, -window.scrollY, duration));
+
+                if (timeElapsed < duration) {
+                    requestAnimationFrame(scrollStep);
+                }
+            }
+
+            requestAnimationFrame(scrollStep);
+        }
